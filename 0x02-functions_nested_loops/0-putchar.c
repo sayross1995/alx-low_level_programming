e3adc8c206080df9fdbfd1777197c1917de98e01
sayross1995 <mohamedsaiss95@gmail.com>
2023-06-20 13:49:43 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,22 @@
 #include "main.h"
-
 /**
- *main - Entry point
- *@c: The character to print
- *Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * * Return: 0
  */
 
-int _putchar(char c);
+
 int main(void)
-{
-
-	print("_putchar\n", strlen("_putchar\n"));
-	return (0);
+	{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
-/**
- * print - writes the array of chars to the stdout
- *@array: the string to print
- *@length: the length of the array defined by the function "strlen"
- * Return: on success 1.
- */
-int print(char *array, int length)
-{
-	return (write(1, &array[0], length));
-}
