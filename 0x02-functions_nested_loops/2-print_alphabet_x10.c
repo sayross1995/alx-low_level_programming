602df62e460e1dcc41c5453224283af014fa0673
sayross1995 <mohamedsaiss95@gmail.com>
2023-06-20 16:31:52 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,23 @@
 #include "main.h"
-/**
- *print_alphabet - prints the alphabet, in lowercase, followed by a new line.
- *
- *Return: always 1(Success)
- */
-void print_alphabet(void)
-{
-	int i = 97;
 
-	do {
-		_putchar(i);
-		i++;
-	} while (i < 123);
-	_putchar('\n');
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
 }
