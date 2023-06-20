0a774ab62c7513b4b9c0221f68c4e9b244e29c39
sayross1995 <mohamedsaiss95@gmail.com>
2023-06-20 08:40:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,3 +14,13 @@ int main(void)
 	return (0);
 }
 
+/**
+ * print - writes the array of chars to the stdout
+ *@array: the string to print
+ *@length: the length of the array defined by the function "strlen"
+ * Return: on success 1.
+ */
+int print(char *array, int length)
+{
+	return (write(1, &array[0], length));
+}
