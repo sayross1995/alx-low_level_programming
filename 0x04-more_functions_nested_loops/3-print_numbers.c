304ef34d058a3ce3fce1f128efa3a77d517a19fa
sayross1995 <mohamedsaiss95@gmail.com>
2023-06-22 07:29:14 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	int c;
+	int  c;
 
-for (c = 0 ; c <= 9 ; c++)
-{
-putchar(c + ‘0’);
-}
-putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	_putchar('\n');
 }
