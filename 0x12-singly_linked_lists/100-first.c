c059408b53a07c032530b5ce6d5b4da52ebc0c6d
sayross1995 <mohamedsaiss95@gmail.com>
2023-07-27 17:57:36 -0700

    jo

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+void _main(void) __attribute__ ((constructor));
+/**
+  * _main - function is executed
+  */
+void _main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
