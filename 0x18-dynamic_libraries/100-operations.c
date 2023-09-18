28e4dcb55193b969808117dce6cfb5879017c6e4
sayross1995 <mohamedsaiss95@gmail.com>
2023-09-18 09:38:31 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ *add - function that does the operation
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: the operation's result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *sub - function that does the operation
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: the operation's result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *mul - function that does the operation
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: the operation's result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *div - function that does the operation
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: the operation's result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *mod - function that does the operation
+ *@a: 1st number
+ *@b: 2nd number
+ *Return: the operation's result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
